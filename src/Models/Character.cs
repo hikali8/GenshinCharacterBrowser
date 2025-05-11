aea25f38ffc6986104c9ff67f7ecb2ce5c0f58f8
hikali8 <90106349+hikali8@users.noreply.github.com>
2025-05-11 23:51:50 +0800

    Update Character.cs

--- src/Models/Character.cs
+++ src/Models/Character.cs
@@ -1,4 +1,4 @@
-﻿using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Linq;
 using System.Linq;
 
 namespace GenshinCharacterBrowser.Models;
@@ -16,11 +16,22 @@ public class Character
 
     public Character(JToken obj)
     {
+        Name = obj["sTitle"].ToString();
+        var sExt = JToken.Parse(obj["sExt"].ToString());
+        IconUrl = sExt["732_0"][0]["url"].ToString();
+        ProtraitUrl = sExt["732_1"][0]["url"].ToString();
+        NameUrl = sExt["732_2"][0]["url"].ToString();
+        ElementUrl = sExt["732_3"][0]["url"].ToString();
+        DialogueUrl = sExt["732_8"][0]["url"].ToString();
+
+        /*
+        //These are the orginal, remained to complement the sense of variables
         Name = obj["title"].ToString();
-        IconUrl = obj["ext"].First(v => v["arrtName"].ToString() == "角色-ICON")["value"][0]["url"].ToString();
+        IconUrl = obj["sExt"].First(v => v["arrtName"].ToString() == "角色-ICON")["value"][0]["url"].ToString();
         ProtraitUrl = obj["ext"].First(v => v["arrtName"].ToString() == "角色-PC端主图")["value"][0]["url"].ToString();
         NameUrl = obj["ext"].First(v => v["arrtName"].ToString() == "角色-名字")["value"][0]["url"].ToString();
         ElementUrl = obj["ext"].First(v => v["arrtName"].ToString() == "角色-属性")["value"][0]["url"].ToString();
         DialogueUrl = obj["ext"].First(v => v["arrtName"].ToString() == "角色-台词")["value"][0]["url"].ToString();
+        */
     }
 }
